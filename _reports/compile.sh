33d0a7e918ed78ad75c3dcf03e01e8113e63a2cf
vantouse <ivcos2003@gmail.com>
2026-05-05 22:12:03 +0500

    replace HTML reports with pure TeX

---
+++ _reports/compile.sh
@@ -0,0 +1,13 @@
+# Compile LaTeX presentation
+# Args:
+#   $1 -- path to project directory
+#   $2 -- TeX-source file name (optional, default: main.tex)
+
+FILE=${2:-main.tex}
+
+latexmk -c -cd "$1/$FILE"
+latexmk -pdflatex='pdflatex -file-line-error -synctex=1 -interaction=nonstopmode' \
+        -pdf \
+        -cd \
+        -outdir=build \
+        "$1/$FILE"
